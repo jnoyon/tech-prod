1663763ad187808b3c23bb9c2c14b3f3f39f1f0e
jnoyon <jihadur51@gmail.com>
2025-04-04 12:18:41 +0600

    Sections Updated

--- src/components/Promotions.jsx
+++ src/components/Promotions.jsx
@@ -4,6 +4,7 @@ import {  FaGift } from 'react-icons/fa';
 import { toast, ToastContainer } from 'react-toastify';
 import Lottie from 'lottie-react';
 import promotion from '../assets/promotion.json'
+import Heading from '../shared/Heading';
 
 export default function Promotions() {
   const [coupons, setCoupons] = useState([]);
@@ -46,37 +47,38 @@ export default function Promotions() {
         theme="light"
       />
       <div className='mx-auto w-11/12'>
+      <Heading heading='Promotions' subHeading='Here are our Coupons'></Heading> 
         <div className="flex gap-5 flex-col md:flex-row items-center">
           <div className="md:w-1/2">
           <Lottie animationData={promotion} loop={true} />
           </div>
           <div className="md:w-1/2">
-          <div className='grid md:grid-cols-2 gap-5'>
-          {coupons.length > 0 ? (
-            coupons.map((coupon) => (
-              <div key={coupon._id} className='bg-white shadow p-3 rounded-md text-center'>
-                <h1 className='md:text-3xl text-2xl font-bold mb-3 items-center gap-3'>
-                  <FaGift className='text-red-500 mx-auto mb-2' /> {coupon.description}
-                </h1>
-                <div className='md:w-1/2 mx-auto text-gray-600'>
-                  <p className='mb-2'>
-                    Get an exclusive <b>{coupon.discountAmount} OFF</b> on all purchases! Simply use the coupon code <b>{coupon.couponCode}</b> at checkout to claim your discount.
-                    But hurry—this limited-time offer expires on <b>{coupon.expireDate}</b>! </p>
-                  <button
-                    className='btn btn-primary text-white my-5 btn-wide'
-                    onClick={() => handleCopyCoupon(coupon.couponCode)}
-                  >
-                    Claim Offer
-                  </button>
+            <div className='grid md:grid-cols-2 gap-5'>
+            {coupons.length > 0 ? (
+              coupons.map((coupon) => (
+                <div key={coupon._id} className='bg-white shadow p-3 rounded-md text-center'>
+                  <h1 className='md:2text-xl text-xl font-bold mb-3 items-center gap-3'>
+                    <FaGift className='text-red-500 mx-auto mb-2' /> {coupon.description}
+                  </h1>
+                  <div className=' text-gray-600'>
+                    <p className='mb-2'>
+                      Get an exclusive <b>{coupon.discountAmount} OFF</b> on all purchases! Simply use the coupon code <b>{coupon.couponCode}</b> at checkout to claim your discount.
+                      But hurry—this limited-time offer expires on <b>{coupon.expireDate}</b>! </p>
+                    <button
+                      className='btn btn-error text-white my-5 '
+                      onClick={() => handleCopyCoupon(coupon.couponCode)}
+                    >
+                      Claim Offer
+                    </button>
+                  </div>
                 </div>
+              ))
+            ) : (
+              <div className='flex justify-center'>
+                <div className="loading loading-lg loading-spinner"></div>
               </div>
-            ))
-          ) : (
-            <div className='flex justify-center'>
-              <div className="loading loading-lg loading-spinner"></div>
-            </div>
-          )}
-        </div>
+            )}
+          </div>
           </div>
         </div>
       </div>
